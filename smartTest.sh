d1744b137a3482df288ae8e23100e6dc0aa2e385
speeddemon574 <speeddemon574@gmail.com>
2016-06-20 17:37:10 -0500

    Update Comments
    Fixed a few spelling errors in my comments that I failed to catch before.

--- smartTest.sh
+++ smartTest.sh
@@ -1,14 +1,14 @@
 #/bin/bash
 # Uses Smartctl (from smartmontools package) to test disk health.
-# Tested on Ubuntu 14.04 LTS, but this script is likely to work on many other distrobutions as well (within reason).
+# Tested on Ubuntu 14.04 LTS, but this script is likely to work on many other distributions as well (within reason).
 # 
-# This script requires that the smartmontools, mailutils, and mailx package to be installed and configured properally.
+# This script requires that the smartmontools, mailutils, and mailx package to be installed and configured properly.
 # 
-# Remember to change the below variables to fit your needs (an email will NOT be sent untill you do this).
-# This script comes with no Wartenty and I am not resposibible for any damage that this script may cause due to modification or misconfiguration
-# It is recomended that you test this script in a safe envirnment (like in a VM) before depolying it to any machines that you care about.
+# Remember to change the below variables to fit your needs (an email will NOT be sent until you do this).
+# This script comes with no Warranty and I am not responsible for any damage that this script may cause due to modification or misconfiguration.
+# It is recommended that you test this script in a safe environment (like in a VM) before deploying it to any machines that you care about.
 #
-# After entering the email address you want the script to email to, make sure you run the script using sudo, and include the aguement "short" or "long" to specify the level of testing you want.
+# After entering the email address you want the script to email to, make sure you run the script using sudo, and include the argument "short" or "long" to specify the level of testing you want.
 # Note you can use this script in cron (to make it run automatically), just make sure it runs as the root user and you specify weather it is a short or long self test.
 
 email=Admin@example.com
@@ -28,7 +28,7 @@ else
     exit 2
 fi
 
-# Pauses the script untill the self test is done
+# Pauses the script until the self test is done
 if [ "$1" = "short" ]
 then
     sleep 5m    # Change this to more time if needed.
@@ -41,10 +41,10 @@ fi
 
 echo "$(date) - $1 SMART self test complete, sending log to administrator" >> $logfile
 
-# Sends email to administrator with test resaults in the message body
+# Sends email to administrator with test results in the message body
 ( smartctl -l selftest /dev/sda; echo "Full SMART status:"; smartctl -a /dev/sda ) | mailx -s "$1 SMART test completed on $(hostname)." $email
 
-#Checks if the email sent properally and records it in the log
+#Checks if the email sent properly and records it in the log
 if [ $? -eq 0 ]
 then
     echo "$(date) - Email to $email sent successfully" >> $logfile
