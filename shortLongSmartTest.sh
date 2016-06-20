47664107cc4cb848586cad4b07dc15ce668d81d1
speeddemon574 <speeddemon574@gmail.com>
2016-06-20 16:47:32 -0500

    Commented and explained the script
    Added a more clear explanation of how to use the script, what systems it can be used on, and how it works. Also cleaned the script up a bit and removed "sudo" in front of the smartctl commands as it was unnecessary (since the script is supposed to be run by root or sudo user in the first place).

--- shortLongSmartTest.sh
+++ shortLongSmartTest.sh
@@ -1,12 +1,22 @@
 #/bin/bash
-# Uses that Smartctl (from smartmontools package) to test disk health
+# Uses Smartctl (from smartmontools package) to test disk health.
+# Tested on Ubuntu 14.04 LTS, but this script is likely to work on many other distrobutions as well (within reason).
+# 
+# This script requires that the smartmontools, mailutils, and mailx package to be installed and configured properally.
+# 
+# Remember to change the below variables to fit your needs (an email will NOT be sent untill you do this).
+# This script comes with no Wartenty and I am not resposibible for any damage that this script may cause due to modification or misconfiguration
+# It is recomended that you test this script in a safe envirnment (like in a VM) before depolying it to any machines that you care about.
+#
+# After entering the email address you want the script to email to, make sure you run the script using sudo, and include the aguement "short" or "long" to specify the level of testing you want.
+# Note you can use this script in cron (to make it run automatically), just make sure it runs as the root user and you specify weather it is a short or long self test.
 
-email=Your Email address
-logfile=/var/log/autoSmartTest.log
+email=Admin@example.com
+logfile=/var/log/autoSmartTest.log          # Where your log file should be stored, you can keep this as default.
 
 touch $logfile # Creates log file
 
-sudo smartctl -t $1 /dev/sda > /dev/null
+smartctl -t $1 /dev/sda > /dev/null
 
 
 # Checks if the command worked correctly.
@@ -18,7 +28,7 @@ else
     exit 2
 fi
 
-
+# Pauses the script untill the self test is done
 if [ "$1" = "short" ]
 then
     sleep 5m    # Change this to more time if needed.
@@ -31,9 +41,10 @@ fi
 
 echo "$(date) - $1 SMART self test complete, sending log to administrator" >> $logfile
 
+# Sends email to administrator with test resaults in the message body
+( smartctl -l selftest /dev/sda; echo "Full SMART status:"; smartctl -a /dev/sda ) | mailx -s "$1 SMART test completed on $(hostname)." $email
 
-( sudo smartctl -l selftest /dev/sda; echo "Full SMART status:"; sudo smartctl -a /dev/sda ) | mailx -s "$1 SMART test completed on $(hostname)." $email
-
+#Checks if the email sent properally and records it in the log
 if [ $? -eq 0 ]
 then
     echo "$(date) - Email to $email sent successfully" >> $logfile
